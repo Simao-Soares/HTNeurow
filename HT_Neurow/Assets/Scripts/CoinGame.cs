b511b05fde9c06ae25ba5c4ee0bd88f0e396ec69
Simao-Soares <44780464+Simao-Soares@users.noreply.github.com>
2021-11-15 19:12:58 +0000

    coins spawn

--- HT_Neurow/Assets/Scripts/CoinGame.cs
+++ HT_Neurow/Assets/Scripts/CoinGame.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Random = System.Random;
 
 public class CoinGame : MonoBehaviour
 {
@@ -10,12 +11,32 @@ public class CoinGame : MonoBehaviour
     public GameObject instructions;
     public GameObject tempScoreUI;
     public GameObject scoreUI;
+    public float coinGameArea = 400f; // length of the side of playable area
+    public float distanceBetween = 50f;
+
+    public GameObject Coin;
+
+    public int numberOfCoins;
 
     public TMP_Text playerScoreText;
     private int playerScore = 0;
     
     public float instructionsTime;
     private bool auxI;
+
+    public class CoinClass{
+        public string CoinID;
+        public GameObject CoinObject;
+
+    }
+    //Array of Coins aka Objectives
+/* 	[System.Serializable]
+	public class CoinList
+	{
+		public CoinClass[] coinClass;
+	} */
+
+	//public CoinList myCoinList = new CoinList();  
 
 
     // Start is called before the first frame update
@@ -24,6 +45,7 @@ public class CoinGame : MonoBehaviour
         instructions.SetActive(false);
         tempScoreUI.SetActive(false);
         auxI = true;
+        GenerateObjectives();
     }
 
     void FixedUpdate()
@@ -41,12 +63,47 @@ public class CoinGame : MonoBehaviour
         auxI = false;
 		instructions.SetActive(false);
         tempScoreUI.SetActive(false);
-        //scoreUI.transform.localPosition = new Vector3(0,450f,0);  //tried to move the score up a bit but didnt work oh well
+        //scoreUI.transform.localPosition = new Vector3(0,450f,0);  //tried to move the score up a bit but didnt work 
 	}
 
     public void Score(){
         playerScore = playerScore + 1;
         this.playerScoreText.text = playerScore.ToString();
+    }
+
+    public static CoinClass FactoryOfCoin(string coinID)
+    {
+        CoinClass newCoin = new CoinClass();
+        newCoin.CoinID = coinID;
+        //Vector3 newCoords = new Vector3(UnityEngine.Random.Range(-coinGameArea/2, coinGameArea/2), 100f, UnityEngine.Random.Range(-coinGameArea/2, coinGameArea/2));
+       
+        //newCoin.CoinObject.transform = newCoords;
+        return newCoin;
+    }
+        
+
+    private void GenerateObjectives(){
+        List<CoinClass> listCoins = new List<CoinClass>();
+
+        for (int i = 0; i < numberOfCoins; i++)
+        {
+            listCoins.Add(FactoryOfCoin(i.ToString()));
+            Vector3 newCoords = new Vector3(UnityEngine.Random.Range(-coinGameArea/2, coinGameArea/2), 100f, UnityEngine.Random.Range(-coinGameArea/2, coinGameArea/2));
+
+            if(listCoins.Count > 0)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    while (Vector3.Distance(listCoins[j].CoinObject.transform.position, newCoords) < distanceBetween)
+                    {
+                        newCoords = new Vector3(UnityEngine.Random.Range(-coinGameArea/2, coinGameArea/2), 100f, UnityEngine.Random.Range(-coinGameArea/2, coinGameArea/2));
+                    }
+                }
+            }
+            GameObject aux = Instantiate(Coin, newCoords, Quaternion.identity);
+            listCoins[i].CoinObject = aux;
+        }
+
     }
 
 
--- HT_Neurow/Assets/Scripts/PickedUp.cs
+++ HT_Neurow/Assets/Scripts/PickedUp.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+
 public class PickedUp : MonoBehaviour
 {
     public EventTrigger.TriggerEvent CoinPickup;
+
+    //private Component renderer;
+    
+
+    private void Start() {
+        
+    }
+
+
     private void OnTriggerEnter(Collider collisionInfo) {
         if (collisionInfo.GetComponent<Collider>().tag == "PlayerPos")
         {
             BaseEventData eventData = new BaseEventData(EventSystem.current);
             this.CoinPickup.Invoke(eventData);
+
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
         }
     }
 }
