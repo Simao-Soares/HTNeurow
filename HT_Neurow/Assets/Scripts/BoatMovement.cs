4f622b37cce40a53dc4d8877bbfe4ca9f4cc6b73
Simao-Soares <simao.pedrosoares@gmail.com>
2021-11-25 02:16:12 +0000

    animation testing

--- HT_Neurow/Assets/Scripts/BoatMovement.cs
+++ HT_Neurow/Assets/Scripts/BoatMovement.cs
@@ -4,98 +4,130 @@ using UnityEngine;
 
 public class BoatMovement : MonoBehaviour
 {
-	Rigidbody rb;
-	public int ControlMethod = 1; // 1 -> BCI   -1 -> HT
+    Rigidbody rb;
+    //public GameManager gm; //ControlMethod: 1 -> BCI   -1 -> HT
+	
 
-  public float rotAngle;  //angle of rotation with 1 press
-  public float spinningTime; //time it takes takes to rotate
+    public float rotAngle;  //angle of rotation with 1 press
+    public float spinningTime; //time it takes takes to rotate
 
-  public float forwardForce; 
+    public float forwardForce; 
 
 
-  bool cooldownActivated;
+    bool cooldownActivated;
   
-  float timeStarted;
-  float timeSinceStarted;
+    float timeStarted;
+    float timeSinceStarted;
 
-  public bool turnLeft = false;
-  public bool turnRight = false;
+    [HideInInspector] public bool turnLeft = false;
+    [HideInInspector] public bool turnRight = false;
+
+    public GameObject leftPaddle;
+    public GameObject rightPaddle;
+
+    public Animator rowAnim; // ver o video do mpto brackeys
+    
 
 
 
-   
 
-  Quaternion rotateB;
-  Quaternion rotateA;
+    Quaternion rotateB;
+    Quaternion rotateA;
 
-  int countl = 0;
-  int countr = 0;
+    Quaternion paddleA;
+    Quaternion paddleB;
+    Quaternion paddleC;
+    Quaternion paddleD;
+
+
+    int countl = 0;
+    int countr = 0;
 
 
     // Start is called before the first frame update
-    void Start()
-    {
-      rb = GetComponent<Rigidbody>(); 
-      rb.AddForce(0, 0, forwardForce, ForceMode.Impulse); //sketchy
+    void Start(){
+
+        Debug.Log(GameManager.ControlMethod);
+        rb = GetComponent<Rigidbody>(); 
+        rb.AddForce(0, 0, forwardForce, ForceMode.Impulse); //sketchy
+
     }
 
-    private void Update() {
-      //checks input + if cooldown is over + selected ontrol method corresponds to the input 
-      if ((Input.GetKey(KeyCode.LeftArrow) && cooldownActivated == false && ControlMethod == 1)||(turnLeft == true && cooldownActivated == false && ControlMethod == -1))
-        {
-          Turn(true); //turn to the left side=true
-          cooldownActivated = true;
-          turnLeft = false;
+    private void Update(){
+
+        //checks input + if cooldown is over + selected ontrol method corresponds to the input 
+        if ((Input.GetKey(KeyCode.LeftArrow) && cooldownActivated == false && GameManager.ControlMethod == 1) ||(turnLeft == true && cooldownActivated == false && GameManager.ControlMethod == -1)){
+            Turn(true); //turn to the left side=true
+            cooldownActivated = true;
+            turnLeft = false;
+            
+            
         }
-      else if ((Input.GetKey(KeyCode.RightArrow) && cooldownActivated == false && ControlMethod == 1)||(turnRight == true && cooldownActivated == false && ControlMethod == -1))
-        {
-          Turn(false); //turn to the right side=false
-          cooldownActivated = true;
-          turnRight = false;
+
+        else if ((Input.GetKey(KeyCode.RightArrow) && cooldownActivated == false && GameManager.ControlMethod == 1) ||(turnRight == true && cooldownActivated == false && GameManager.ControlMethod == -1)){
+            Turn(false); //turn to the right side=false
+            cooldownActivated = true;
+            turnRight = false;
         }
-        if(cooldownActivated) RunCooldownTimer();
 
-      //Change movement direction
-      rb.velocity=Vector3.zero;
-      rb.AddForce(transform.forward * forwardForce, ForceMode.Impulse); 
-		}
+        if(cooldownActivated) RunCooldownTimer(GameManager.ControlMethod);
+
+        //Change movement direction
+        rb.velocity=Vector3.zero;
+        rb.AddForce(transform.forward * forwardForce, ForceMode.Impulse); 
+    }
 
 
 
-    public void Turn(bool side)
-    {
-      cooldownActivated = true;
-      timeStarted = Time.time;
+    public void Turn(bool side){
+        cooldownActivated = true;
+        timeStarted = Time.time;
+        //Transform thisTransform = gameObject.transform;
+        rotateA.eulerAngles = this.transform.rotation.eulerAngles;
+
+        //Debug.Log(rotateA.eulerAngles);
+
+        if(side){
+            countl++;
+            rotateB.eulerAngles = rotateA * new Vector3(0, rotAngle * (countr-countl), 0);
+            //Debug.Log(rotateB.eulerAngles);
+        }
+        else if(side==false){
+            countr++;
+            rotateB.eulerAngles = rotateA * new Vector3(0, rotAngle * (countr-countl), 0);
+            //Debug.Log(rotateB.eulerAngles);
+        }
+    }
+
+
+    void RunCooldownTimer(int auxControl){  //runs until rotation is complete and 
+        float timeSinceStarted = Time.time - timeStarted;
+        float percentageComplete = timeSinceStarted / spinningTime;
       
+        float initY = 0;    //goes to -15 then 15 then back to 0
+        float initZ = -65;  //goes to -65 and back
 
-      Transform thisTransform = gameObject.transform;
+        //paddleA.eulerAngles = new Vector3(0, initY, initZ);
+        //paddleB.eulerAngles = new Vector3(0, -15f, -77.5f);
+        //paddleC.eulerAngles = new Vector3(0, 0, -90);
+        //paddleD.eulerAngles = new Vector3(0, 15f, -77.5f);
 
-      rotateA.eulerAngles = this.transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Lerp(rotateA, rotateB, percentageComplete);
 
-      //Debug.Log(rotateA.eulerAngles);
+        //need to call this inside the if in Update and pass the side as an argument!!
+        //leftPaddle.transform.rotation = Quaternion.Lerp(paddleA, paddleB, percentageComplete);
+        //leftPaddle.transform.rotation = Quaternion.Lerp(paddleB, paddleC, percentageComplete);
+        //leftPaddle.transform.rotation = Quaternion.Lerp(paddleC, paddleD, percentageComplete);
+        //leftPaddle.transform.rotation = Quaternion.Lerp(paddleD, paddleA, percentageComplete);
 
-      if(side){
-        countl++;
-        rotateB.eulerAngles = rotateA * new Vector3(0, rotAngle * (countr-countl), 0);
-        //Debug.Log(rotateB.eulerAngles);
-      }
-      else if(side==false) {
-        countr++;
-        rotateB.eulerAngles = rotateA * new Vector3(0, rotAngle * (countr-countl), 0);
-        //Debug.Log(rotateB.eulerAngles);
-      }
-    }
 
-    void RunCooldownTimer()
-    {
-      float timeSinceStarted = Time.time - timeStarted;
-      float percentageComplete = timeSinceStarted / spinningTime;
+        
 
-      transform.rotation = Quaternion.Lerp(rotateA, rotateB, percentageComplete);
 
-      if (percentageComplete >= 1)
-      {
-          cooldownActivated = false;
-      }
+
+        if (percentageComplete >= 1)
+        {
+            cooldownActivated = false;
+        }
     }
 }
--- HT_Neurow/Assets/Scripts/GameManager.cs
+++ HT_Neurow/Assets/Scripts/GameManager.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-	//for the reload function
-    [SerializeField] private GameObject zone;
+    public GameObject zone; //interaction zone
+
+    //for the reload function
     public GameObject water;
     public GameObject player;
+    
 
+    //-------------------------------------------------- GAME SETTINGS --------------------------------------------------
 
-	public GameObject myPlayer;
-	public BoatMovement _playerScript;
+    public static int ControlMethod = 1; //static -> instances of GameObject will share this value 
+                                         //  1 -> BCI (arrowKeys)
+                                         // -1 -> HT (leapMotion)
 
+    public static int HemiLimb = 0;      //  0 -> No hemiparethic limb
+                                         //  1 -> Right hemiparethic limb
+                                         // -1 -> Left hemiparethic limb
 
+    public static int Gender = 1;        //  1 -> Male
+                                         // -1 -> Female
+
+    public static int Forward = 1;       //  1 -> Auto (always moving forward)
+                                         // -1 -> Manual (forward movement based on rowing)
+
+    //-------------------------------------------------------------------------------------------------------------------
 
 
     // Start is called before the first frame update
     void Start()
     {
-		_playerScript = myPlayer.GetComponent<BoatMovement>();
+		
         //trying to solve reflections bug:
         //reloadAssets(); 
     }
@@ -44,11 +58,26 @@ public class GameManager : MonoBehaviour
     }
 
 
-	void SetBCIControlMethod(){
-		_playerScript.ControlMethod = 1;
+	public void SetBCIControlMethod(){
+		ControlMethod = 1;
 	}
 
-	void SetHTControlMethod(){
-		_playerScript.ControlMethod = -1;
+	public void SetHTControlMethod(){
+		ControlMethod = -1;
 	}
+
+    public void SetHemiLimb(int HemiAux)
+    {
+        HemiLimb = HemiAux;
+    }
+
+    public void SetGender(int GenderAux)
+    {
+        Gender = GenderAux;
+    }
+
+    public void SetForward(int ForwardAux)
+    {
+        Forward = ForwardAux;
+    }
 }
